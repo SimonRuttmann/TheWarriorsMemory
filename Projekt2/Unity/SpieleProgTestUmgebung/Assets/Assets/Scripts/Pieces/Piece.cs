2331bcc491542f27401104e35b093e263439efc4
Ulrich Michael <ulrich1999@web.de>
2022-06-19 01:43:21 +0200

    Moved scene around, fixed multiple a bug where a player could hit itself and changed ground color

--- Projekt2/Unity/SpieleProgTestUmgebung/Assets/Assets/Scripts/Pieces/Piece.cs
+++ Projekt2/Unity/SpieleProgTestUmgebung/Assets/Assets/Scripts/Pieces/Piece.cs
@@ -281,6 +281,8 @@ namespace Scripts.Pieces
 		
 		public ISet<Hexagon> GenerateAllPossibleMovements()
 		{
+			_allPossibleMovements.Clear();
+			
 			var attacks = GeneratePossibleAttackMovements();
 			var moves = GeneratePossibleMoveMovements();
 			
