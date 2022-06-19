bf14c6bceeed338387713ba2efef79020e13c67f
simon.ruttmann <simon.ruttmann@freenet.de>
2022-06-19 03:18:45 +0200

    feature(RotationCalculator): Calculation of degrees

--- Projekt2/Unity/SpieleProgTestUmgebung/Assets/Assets/Scripts/InGameLogic/RotationCalculator.cs
+++ Projekt2/Unity/SpieleProgTestUmgebung/Assets/Assets/Scripts/InGameLogic/RotationCalculator.cs
@@ -1,5 +1,4 @@
 using System;
-using Scripts.Enums;
 using Scripts.GameField;
 using Scripts.Pieces.Interfaces;
 using Scripts.Toolbox;
@@ -39,29 +38,69 @@ namespace Scripts.InGameLogic
         /// <returns>The degree between the piece and the destination</returns>
         public static float ResolveRotationToPosition(IGameFieldManager gameFieldManager, IPiece piece, Hexagon destination)
         {
-            
+            Debug.Log("CALCULATION:");
             var piecePosition = gameFieldManager.ResolveAbsolutePositionOfHexagon(piece.Position);
             var destinationPosition = gameFieldManager.ResolveAbsolutePositionOfHexagon(destination);
-
+            Debug.Log("Hexagons: " + piece.Position.PosX + "|" + piece.Position.PosY + " , " + destination.PosX + "|" + destination.PosY);
+            Debug.Log("Positions: " + piecePosition + " , " +  destinationPosition);
+            
             var degree =  GetDegreeFromPointAToBZeroedToRight(piecePosition, destinationPosition);
+            Debug.Log("degree: " + degree);
+        //    var adjustedDegree = piece.Team == Team.Enemy ? 
+        //        degree + 270 :       //Enemy looks to left
+        //        degree + 90;          //Player looks to right
             
-            var adjustedDegree = piece.Team == Team.Enemy ? 
-                degree + 270 :       //Enemy looks to left
-                degree + 90;         //Player looks to right
-            
-            return (float) adjustedDegree;
+        //    return (float) adjustedDegree;
+        return (float)degree;
         }
         
         private static double GetDegreeFromPointAToBZeroedToRight(Vector3 a, Vector3 b)
         {
+            
+            var oppositeSide = b.x - a.x;
+            var adjacentSide = b.z - a.z;
 
-            var oppositeSide = b.y - a.y;
-            var adjacentSide = b.x - b.y;
+            if (adjacentSide == 0) return GetDegreeFromPointAToBSameZ(a, b);
             
             var radians = Math.Atan((oppositeSide) / (adjacentSide));   //Radian
-            return (180 / Math.PI) * radians;                           //Degree
+            if (radians < 0) radians += 2 * Math.PI;
             
+            var degree =  (180 / Math.PI) * radians;                    //Degree
+
+            if (a.z > b.z) degree += 180;
+            
+            //If the opposite Side and | or the adjacent Side is below 0,
+            //it is necessary to add or subtract 180 degree,
+
+            //Case 1
+            //E.g. A to B is the arcTan(2-1 / 2-1) --> 45 deg
+            
+            //     B to A is the arcTan(1-2 / 1-2) --> 45 deg => Adjustment required
+
+            //              B (2,2) 
+            // A (1,1) 
+
+
+            //Case 2
+            //E.g. A to B is the arcTan(1- -2 /  1 - -2) -> arcTan(1) --> 45 deg
+            //     B to A is the arcTan(-2 -1 / -2 -1 )  -> arcTan(1) --> 45 deg => Adjustment required
+
+            //  B (1,1)         
+            //              A (-2,-2) 
+
+
+
+      //       var adjustedDegree = adjacentSide < 0 || oppositeSide < 0 ? degree + 180 : degree;
+      //       return adjustedDegree;
+            return degree;
         }
+
+        private static double GetDegreeFromPointAToBSameZ(Vector3 a, Vector3 b)
+        {
+            Debug.Log("Degree same X");
+            // A --> B          B <-- A
+            return a.x < b.x ? 90 : 270;
+        }    
 
     }
 }
