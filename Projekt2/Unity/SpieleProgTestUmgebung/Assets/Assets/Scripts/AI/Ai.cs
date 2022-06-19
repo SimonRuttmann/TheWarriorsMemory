b150b1dd27a6afdba4da40ce949775c655a36f00
Ulrich Michael <ulrich1999@web.de>
2022-06-19 14:35:04 +0200

    Fixed bug when ai had no available moves, made ai smarter

--- Projekt2/Unity/SpieleProgTestUmgebung/Assets/Assets/Scripts/AI/Ai.cs
+++ Projekt2/Unity/SpieleProgTestUmgebung/Assets/Assets/Scripts/AI/Ai.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Scripts.GameField;
+using Scripts.Pieces;
 using Scripts.Pieces.Interfaces;
+using UnityEngine;
 
 namespace Scripts.AI
 {
@@ -10,20 +13,60 @@ namespace Scripts.AI
     {
         public Hexagon GetAiMove(GameFieldManager gameFieldManager, IList<IPiece> playerPieces, IPiece ownPiece)
         {
-
             //Stupid ai, just for test..
             //With the gameFieldManager, the whole game field can easily transversed
             
             var attackMovements = ownPiece.GeneratePossibleAttackMovements();
             if (attackMovements.Count > 0) 
-                return attackMovements.First();
+                return GetPriorityAttack(attackMovements);
             
             var moveMovements = ownPiece.GeneratePossibleMoveMovements();
             if (moveMovements.Count > 0)
-                return moveMovements.First();
+                return GetHexagonClosestToEnemy(moveMovements, playerPieces);
 
-            //TODO The ai cant do any move
-            throw new ArgumentNullException();
+            return null;
         }
+
+        private Hexagon GetHexagonClosestToEnemy(ISet<Hexagon> possibleMoves, IList<IPiece> playerPieces)
+        {
+            KeyValuePair<int, Hexagon> closestMove = new KeyValuePair<int, Hexagon>(0, null);
+            foreach (var possibleMove in possibleMoves)
+            {
+                foreach (var playerPiece in playerPieces)
+                {
+                    var distance = CalculateDistance(possibleMove, playerPiece.Position);
+                    if (closestMove.Value == null || closestMove.Key > distance)
+                    {
+                        closestMove = new KeyValuePair<int, Hexagon>(distance, possibleMove);
+                    }
+                }
+            }
+            return closestMove.Value;
+        }
+        private int CalculateDistance(Hexagon hex1,Hexagon hex2)
+        {
+            var number1 = hex1.PosX - hex2.PosX;
+            var number2 = hex1.PosY - hex2.PosY;
+            
+            if (number1 < 0) number1 = -number1;
+            if (number2 < 0) number2 = -number2;
+
+            return number1 + number2;
+        }
+        private Hexagon GetPriorityAttack(ISet<Hexagon> possibleAttacks)
+        {
+            KeyValuePair<int, Hexagon> priorityAttack = new KeyValuePair<int, Hexagon>(0,null);
+            foreach (var possibleAttack in possibleAttacks)
+            {
+                var piece = possibleAttack.Piece;
+                var priority = piece.Health - piece.AttackDamage;
+
+                if (priorityAttack.Value == null || priorityAttack.Key > priority)
+                    priorityAttack = new KeyValuePair<int, Hexagon>(priority, possibleAttack);
+            }
+
+            return priorityAttack.Value;
+        }
+        
     }
 }
